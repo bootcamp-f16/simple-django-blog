56271f0df43ad6b63c01b75997ee488f1685e247
Matt Smith <msmith@apaxsoftware.com>
2016-09-18 23:58:04 -0400

    Improve view tests.

--- blog/test_views.py
+++ blog/test_views.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import reverse
+from django.utils.text import slugify
 from django.test import TestCase
 
 from .models import Post
@@ -7,9 +8,20 @@ from .models import Post
 class BlogViewTests(TestCase):
     def setUp(self):
         self.index_url = reverse('blog:index')
+        self.post_title = "Post title"
+        self.post = Post(title=slugify(self.post_title))
+        self.post.save()
 
     def test_views_index(self):
         response = self.client.get(self.index_url)
 
         self.assertEqual(response.status_code, 200)
+        self.assertTrue('posts' in response.context)
         self.assertIn('blog/index.html', [template.name for template in response.templates])
+
+    def test_views_post(self):
+        response = self.client.get(self.post.get_absolute_url())
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.post, response.context['post'])
+        self.assertIn('blog/post.html', [template.name for template in response.templates])
